f91c044d4a4466044db367da129511f88a359936
Angel Martinez <angel.m.97@hotmail.com>
2024-01-25 16:54:13 -0600

    Initial Commit

---
+++ BBQuotes/BBQuotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  BBQuotesApp.swift
+//  BBQuotes
+//
+//  Created by Angel Martínez Escobar on 25/01/24.
+//
+
+import SwiftUI
+
+@main
+struct BBQuotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
